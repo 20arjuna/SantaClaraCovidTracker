a065627a413ff5c3d95e6c551ca383b4a346d449
Arjun Akkiraju <20arjuna@students.harker.org>
2020-03-25 10:22:11 -0700

    basic plotting from text files

--- graph.py
+++ graph.py
@@ -1,4 +1,18 @@
-import matplotlib
+import matplotlib.pyplot as p
 
 daysFile = open("days.txt", "r")
 infectedFile = open("infected.txt", "r")
+
+daysLines = daysFile.readlines()
+infectedLines = infectedFile.readlines()
+
+days = []
+infected = []
+
+for line in daysLines:
+     days.append(int(line))
+for line in infectedLines:
+    infected.append(int(line))
+
+p.plot(days, infected)
+p.show()
