265f1059037db404027efc3fb88d6712ded0391a
Arjun Akkiraju <20arjuna@students.harker.org>
2020-03-25 14:47:58 -0700

    able to get cases off of santa clara website

--- graph.py
+++ graph.py
@@ -1,24 +1,41 @@
 import matplotlib.pyplot as p
 import numpy as np
-daysFile = open("days.txt", "r")
-infectedFile = open("infected.txt", "r")
+import requests
 
-daysLines = daysFile.readlines()
-infectedLines = infectedFile.readlines()
+def plotGraph():
 
-days = []
-infected = []
+    daysFile = open("days.txt", "r")
+    infectedFile = open("infected.txt", "r")
 
-for line in daysLines:
-     days.append(line.rstrip("\n"))
+    daysLines = daysFile.readlines()
+    infectedLines = infectedFile.readlines()
 
-for line in infectedLines:
-    infected.append(int(line))
+    days = []
+    infected = []
 
-p.plot(days, infected)
-p.xlabel('Date')
-p.ylabel('Total Infected')
-p.title('COVID-19 cases in Santa Clara County')
+    for line in daysLines:
+        days.append(line.rstrip("\n"))
 
-p.xticks(np.arange(0, len(days), step=14))
-p.show()
+    for line in infectedLines:
+        infected.append(int(line))
+
+    p.plot(days, infected)
+    p.xlabel('Date')
+    p.ylabel('Total Infected')
+    p.title('COVID-19 cases in Santa Clara County')
+
+    p.xticks(np.arange(0, len(days), step=14))
+    p.show()
+
+def getCases():
+    r = requests.get("https://www.sccgov.org/sites/phd/DiseaseInformation/novel-coronavirus/Pages/home.aspx")
+    sourceCode = r.text
+    index = sourceCode.find("Total_Confirmed_Cases")
+    cases = int(sourceCode[index+24:index+27])
+    return cases
+
+
+if __name__ == '__main__':
+    cases = getCases()
+    print(cases)
+    plotGraph()
