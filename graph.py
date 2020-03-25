78630c8884a00b64f1dcf51e2f261458380b5e85
Arjun Akkiraju <20arjuna@students.harker.org>
2020-03-25 13:42:58 -0700

    updates axes and ticks

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as p
-
+import numpy as np
 daysFile = open("days.txt", "r")
 infectedFile = open("infected.txt", "r")
 
@@ -10,12 +10,15 @@ days = []
 infected = []
 
 for line in daysLines:
-     days.append(int(line))
+     days.append(line.rstrip("\n"))
+
 for line in infectedLines:
     infected.append(int(line))
 
 p.plot(days, infected)
-p.xlabel('Days since 1st case')
+p.xlabel('Date')
 p.ylabel('Total Infected')
 p.title('COVID-19 cases in Santa Clara County')
+
+p.xticks(np.arange(0, len(days), step=14))
 p.show()
